868220864734b6572918418c3f1fc8b63dd6ccdb
abhishektarun09 <abhishek.tarun09@gmail.com>
2025-05-05 23:37:19 +0200

    Included MySQL integration and .env

--- app.py
+++ app.py
@@ -8,25 +8,52 @@ from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
+from dotenv import load_dotenv
+load_dotenv()
+
 from babel.numbers import format_currency
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import CSRFProtect
 
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 from src.utils import apology, login_required
 
 app = Flask(__name__)
-## Route for a home page
+app.secret_key = os.getenv("SECRET_KEY")
+
+user = os.getenv('MYSQL_USER')
+password = os.getenv('MYSQL_PASSWORD')
+host = os.getenv('MYSQL_HOST')
+port = os.getenv('MYSQL_PORT')
+db_name = os.getenv('MYSQL_DB')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    hash = db.Column(db.String(500), nullable=False)
+
+with app.app_context():
+    db.create_all()
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+csrf = CSRFProtect(app)
+
 @app.route('/')
 def index():
     return render_template('index.html', current_year = datetime.now().year) 
 
 @app.route('/prediction', methods = ['GET','POST'])
-#@login_required
+@login_required
 def predict():
     if request.method == 'GET':
         return render_template('prediction.html')
@@ -75,21 +102,17 @@ def login():
             return apology("must provide password", 403)
 
         # Query database for username
-        rows = db.execute(
-            "SELECT * FROM users WHERE username = ?", request.form.get("username")
-        )
+        user = User.query.filter_by(username=request.form.get("username")).first()
 
         # Ensure username exists and password is correct
-        if len(rows) != 1 or not check_password_hash(
-            rows[0]["hash"], request.form.get("password")
-        ):
+        if not user or not check_password_hash(user.hash, request.form.get("password")):
             return apology("invalid username and/or password", 403)
 
         # Remember which user has logged in
-        session["user_id"] = rows[0]["id"]
+        session["user_id"] = user.id
 
         # Redirect user to home page
-        return redirect("/predict")
+        return redirect("/prediction")
 
     # User reached route via GET (as by clicking a link or via redirect)
     else:
@@ -102,6 +125,8 @@ def logout():
 
     # Forget any user_id
     session.clear()
+    
+    flash("You have logged out successfully.")
 
     # Redirect user to login form
     return redirect("/")
@@ -133,18 +158,21 @@ def register():
         hash = generate_password_hash(request.form.get("password"))
 
         try:
-            # Store credentials in the database
-            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)",
-                       request.form.get("username"), hash)
+            # Check if username already exists
+            existing_user = User.query.filter_by(username=request.form.get("username")).first()
+            if existing_user:
+                return apology("think of another username", 400)
 
-            result = db.execute("SELECT * FROM users WHERE username = ?",
-                                request.form.get("username"))
+            # Create and add user
+            new_user = User(username=request.form.get("username"), hash=hash)
+            db.session.add(new_user)
+            db.session.commit()
 
             # Remember which user has logged in
-            session["user_id"] = result[0]["id"]
+            session["user_id"] = new_user.id
 
             # Redirect user to home page
-            return redirect("/predict")
+            return redirect("/prediction")
 
         except:
             # Username already exists
@@ -182,24 +210,16 @@ def change_password():
         elif not (request.form.get("new_password") != request.form.get("old_password")):
             return apology("enter new password", 403)
 
-        # Query database for PASSWORD
-        rows = db.execute(
-            "SELECT * FROM users WHERE id = ?", session["user_id"]
-        )
+        # Query database for User details
+        user = User.query.get(session["user_id"])
 
         # Ensure old password is correct
-        if not check_password_hash(
-            rows[0]["hash"], request.form.get("old_password")
-        ):
+        if not user or not check_password_hash(user.hash, request.form.get("old_password")):
             return apology("must provide valid old password", 403)
 
-        # Hash the new password
-        hash = generate_password_hash(request.form.get("confirmation_password"))
-
-        # Query database to store new PASSWORD
-        db.execute(
-            "UPDATE users SET hash = ? WHERE id = ?", hash, session["user_id"]
-        )
+        # Hash new password and update
+        user.hash = generate_password_hash(request.form.get("confirmation_password"))
+        db.session.commit()
 
         flash("Password Changed!")
 
