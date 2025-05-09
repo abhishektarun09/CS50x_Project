7f1ec032a13f8e34162850dbe5f22f3dbff0a435
abhishektarun09 <abhishek.tarun09@gmail.com>
2025-05-09 23:53:16 +0200

    Test cert path

--- app.py
+++ app.py
@@ -29,24 +29,28 @@ password = os.getenv("MYSQL_PASSWORD")
 host = os.getenv("MYSQL_HOST")
 port = os.getenv("MYSQL_PORT")
 db_name = os.getenv("MYSQL_DB")
-ssl_ca_path = os.getenv("SSL_CA_PATH")
 
-'''app.config['SQLALCHEMY_DATABASE_URI'] = (
+#Cert file path (test)
+base_dir = os.path.dirname(os.path.abspath(__file__))
+ssl_ca_path = os.path.join(base_dir, "cert", "DigiCertGlobalRootCA.crt.pem")
+#ssl_ca_path = os.getenv("SSL_CA_PATH")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = (
     f"mysql+pymysql://{user}:{password}@{host}:{port}/{db_name}"
     f"?ssl_ca={ssl_ca_path}"
 )
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False'''
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-'''class User(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
-    hash = db.Column(db.String(500), nullable=False)'''
+    hash = db.Column(db.String(500), nullable=False)
 
-'''with app.app_context():
+with app.app_context():
     print("SSL_CA_PATH used:", ssl_ca_path)
-    db.create_all()'''
+    db.create_all()
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
